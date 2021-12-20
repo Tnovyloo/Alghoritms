189ed5f0bf4126aa21d8bac26901964201833d8b
Tomasz <loobrome@gmail.com>
2021-12-20 19:26:06 +0100

    Adding simple power recursion

---
+++ Potega_Rekurencja.py
@@ -0,0 +1,7 @@
+def power(base, exp):
+    if exp == 1:
+        return base
+    if exp != 1:
+        return base * power(base, exp-1)
+
+print(power(base=int(input('Podaj liczbe: ')), exp=int(input('Podaj wykładnik: '))))
